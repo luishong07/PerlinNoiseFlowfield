6b490e74623d837b8dc4dde9862f6afbd5548411
Luis Hong <luishong50@gmail.com>
2023-03-28 07:54:02 -0500

    redo

--- sketch.js
+++ sketch.js
@@ -1,54 +1,55 @@
-let increment = 5;
-let scale = 25;
-let cols, rows;
-let zOff =0
-let particles = []
+// let increment = 10;
+// let scale = 25;
+// let cols, rows;
+// let zOff =0
+// let particles = []
 
-let flowField 
+// let flowField 
 function setup() {
     // createCanvas(500, 500);
     createCanvas(innerWidth, innerHeight)
-    cols = floor(width / scale);
-    rows = floor(height / scale);
-    // put setup code here
-    flowField = new Array(cols*rows)
-	for(let i = 0; i < 500; i++){
+    // cols = floor(width / scale);
+    // rows = floor(height / scale);
+    // // put setup code here
+    // flowField = new Array(cols*rows)
+	// for(let i = 0; i < 100; i++){
 
-		particles[i]=  new Particle()
-	}
-    background(255);
+	// 	particles[i]=  new Particle()
+	// }
 }
 
 
 function draw() {
+    background(255);
     // put drawing code here
-    let yOff = 0;
-    for (let y = 0; y < rows; y++) {
-        let xOff = 0;
-        for (let x = 0; x < cols; x++) {
-            let index = (x + y * cols);
-            let angle = noise(xOff, yOff,zOff) * TWO_PI * 2;
-            let v = p5.Vector.fromAngle(angle);
-            // v.setMag(0.1)
-            flowField[index] = v
-            xOff += increment;
-            // stroke(0);
-            // push();
-            // translate(x * scale, y * scale);
-            // rotate(v.heading());
-            // line(0, 0, scale, 0);
-            // // ellipse(scale,0, 5,5)
-            // pop();
-        }
-        yOff += increment;
-		zOff+=0.0001
-    }
-	for(let i = 0; i < particles.length;i++){
-        particles[i].follow(flowField)
-        particles[i].update()
-        particles[i].edges()
-		particles[i].show()
-		// particles[i].applyForce()
-	}
+    // let yOff = 0;
+    // for (let y = 0; y < rows; y++) {
+    //     let xOff = 0;
+    //     for (let x = 0; x < cols; x++) {
+    //         let index = (x + y * width)*4;
+    //         let angle = noise(xOff, yOff) * TWO_PI ;
+    //         // console.log(angle)
+    //         let v = p5.Vector.fromAngle(angle);
+    //         // v.setMag(0.1)
+    //         // flowField[index] = v
+    //         xOff += increment;
+    //         stroke(0);
+    //         push();
+    //         translate(x * scale, y * scale);
+    //         rotate(v.heading());
+    //         line(0, 0, scale, 0);
+    //         // ellipse(scale,0, 5,5)
+    //         pop();
+    //     }
+    //     yOff += increment;
+	// 	zOff+=0.0001
+    // }
+	// for(let i = 0; i < particles.length;i++){
+    //     particles[i].follow(flowField)
+    //     particles[i].update()
+    //     particles[i].edges()
+	// 	particles[i].show()
+	// 	// particles[i].applyForce()
+	// }
 
 }
