14d72cb72952c79c400845ae8ce181915a051763
Luis Hong <luishong50@gmail.com>
2023-03-28 08:40:50 -0500

    managed to recreate static field, just the lines no particles or motion

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-// let increment = 10;
-// let scale = 25;
-// let cols, rows;
+let increment = 0.1;
+let scale = 25;
+let cols, rows;
 // let zOff =0
 // let particles = []
 
@@ -8,8 +8,8 @@
 function setup() {
     // createCanvas(500, 500);
     createCanvas(innerWidth, innerHeight)
-    // cols = floor(width / scale);
-    // rows = floor(height / scale);
+    cols = floor(width / scale);
+    rows = floor(height / scale);
     // // put setup code here
     // flowField = new Array(cols*rows)
 	// for(let i = 0; i < 100; i++){
@@ -21,29 +21,28 @@ function setup() {
 
 function draw() {
     background(255);
-    // put drawing code here
-    // let yOff = 0;
-    // for (let y = 0; y < rows; y++) {
-    //     let xOff = 0;
-    //     for (let x = 0; x < cols; x++) {
-    //         let index = (x + y * width)*4;
-    //         let angle = noise(xOff, yOff) * TWO_PI ;
-    //         // console.log(angle)
-    //         let v = p5.Vector.fromAngle(angle);
-    //         // v.setMag(0.1)
-    //         // flowField[index] = v
-    //         xOff += increment;
-    //         stroke(0);
-    //         push();
-    //         translate(x * scale, y * scale);
-    //         rotate(v.heading());
-    //         line(0, 0, scale, 0);
-    //         // ellipse(scale,0, 5,5)
-    //         pop();
-    //     }
-    //     yOff += increment;
-	// 	zOff+=0.0001
-    // }
+    let yOff = 0;
+    for (let y = 0; y < rows; y++) {
+        let xOff = 0;
+        for (let x = 0; x < cols; x++) {
+            let index = (x + y * width)*4;
+            let angle = noise(xOff, yOff) * TWO_PI ;
+            let v = p5.Vector.fromAngle(angle);
+            // v.setMag(0.1)
+            // flowField[index] = v
+            xOff += increment;
+            fill(random(255))
+            stroke(0);
+            push();
+            translate(x * scale, y * scale);
+            rotate(v.heading());
+            line(0, 0, scale, 0);
+            // // ellipse(scale,0, 5,5)
+            pop();
+        }
+        yOff += increment;
+		// zOff+=0.0001
+    }
 	// for(let i = 0; i < particles.length;i++){
     //     particles[i].follow(flowField)
     //     particles[i].update()
