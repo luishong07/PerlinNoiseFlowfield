a001763292b094e6456282ab177636256d9da097
Luis Hong <luishong50@gmail.com>
2023-03-28 07:34:19 -0500

    simple testing

--- sketch.js
+++ sketch.js
@@ -1,47 +1,45 @@
-let increment = 5;
-let scale = 25;
+let increment = 10;
+let scale = 100;
 let cols, rows;
 let zOff =0
 let particles = []
 
 let flowField 
 function setup() {
-    // createCanvas(500, 500);
     createCanvas(innerWidth, innerHeight)
     cols = floor(width / scale);
     rows = floor(height / scale);
-    // put setup code here
     flowField = new Array(cols*rows)
-	for(let i = 0; i < 500; i++){
+	for(let i = 0; i < 50; i++){
 
 		particles[i]=  new Particle()
 	}
-    background(255);
 }
 
 
 function draw() {
+    background(255);
     // put drawing code here
     let yOff = 0;
     for (let y = 0; y < rows; y++) {
         let xOff = 0;
         for (let x = 0; x < cols; x++) {
-            let index = (x + y * cols);
-            let angle = noise(xOff, yOff,zOff) * TWO_PI * 2;
+            let index = (x + y * cols) ;
+            let angle = noise(xOff, yOff,zOff)* PI*2 ;
             let v = p5.Vector.fromAngle(angle);
             // v.setMag(0.1)
             flowField[index] = v
             xOff += increment;
-            // stroke(0);
-            // push();
-            // translate(x * scale, y * scale);
-            // rotate(v.heading());
-            // line(0, 0, scale, 0);
-            // // ellipse(scale,0, 5,5)
-            // pop();
+            stroke(0);
+            push();
+            translate(x * scale, y * scale);
+            rotate(v.heading());
+            line(0, 0, scale, 0);
+            // ellipse(scale,0, 5,5)
+            pop();
         }
         yOff += increment;
-		zOff+=0.0001
+		zOff+=0.0005 
     }
 	for(let i = 0; i < particles.length;i++){
         particles[i].follow(flowField)
